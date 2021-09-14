5e411eec90b7b87d4cd7043c62d125fe09e6b41c
Bhargav <bhargavkadali39@gmail.com>
2021-09-14 15:51:46 +0530

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -1,5 +1,4 @@
 email = input("Enter Your Email: ").strip()
 username = email[:email.index("@")]
 domain_name = email[email.index("@")+1:]
-format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
-print(format_)
+print(f"Your user name is '{username}' and your domain is '{domain_name}'")
